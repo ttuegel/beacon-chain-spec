e235ec7d5e00f8e262d0a4b8cb2c3397b7a95457
Daejun Park <daejunpark@gmail.com>
2019-06-21 12:45:48 -0500

    add block root

--- abstract/run.sh
+++ abstract/run.sh
@@ -3,3 +3,4 @@
   rm -rf abstract-beacon-chain-kompiled/
   kompile --backend java abstract-beacon-chain.k
   krun --smt none test1.abc | diff - test1.abc.out
+# krun --smt none test1.abc > test1.abc.out
