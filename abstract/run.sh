c46d3ecf8f40391dec95f8610ac5c72008390ab2
Daejun Park <daejunpark@gmail.com>
2019-06-24 14:48:58 -0500

    justification

--- abstract/run.sh
+++ abstract/run.sh
@@ -3,4 +3,9 @@
   rm -rf abstract-beacon-chain-kompiled/
   kompile --backend java abstract-beacon-chain.k
   krun --smt none test1.abc | diff - test1.abc.out
-# krun --smt none test1.abc > test1.abc.out
+  krun --smt none test2.abc | diff - test2.abc.out
+  krun --smt none test3.abc | diff - test3.abc.out
+
+# krun --smt none test1.abc >test1.abc.out
+# krun --smt none test2.abc >test2.abc.out
+# krun --smt none test3.abc >test3.abc.out
